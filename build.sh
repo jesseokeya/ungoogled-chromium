9ee1e5706ee51e86c8a9afe3801a50c5f8618807
jesseokeya <jesseokeya@gmail.com>
2019-11-15 22:44:29 -0500

    changes

--- build.sh
+++ build.sh
@@ -1,1 +1,4 @@
-git submodule update --init --recursive
+git submodule add --force https://github.com/Eloston/ungoogled-chromium.git archlinux/ungoogled-chromium
+git submodule add --force https://github.com/Eloston/ungoogled-chromium.git debian/ungoogled-chromium
+git submodule add --force https://github.com/Eloston/ungoogled-chromium.git macos/ungoogled-chromium
+git submodule add --force https://github.com/Eloston/ungoogled-chromium.git windows/ungoogled-chromium
